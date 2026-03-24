4412b35582eb53e631faf80d01fbe1b4b16ef3c3
imad-shah <ishah5@student.gsu.edu>
2026-03-24 13:28:34 -0400

    refactor and cleanup

--- game/main.py
+++ game/main.py
@@ -4,6 +4,7 @@ import pygame
 SCREEN_LEN = 1097
 SCREEN_WID = 744
 FPS = 120
+PLAYER_SPEED = 2
 
 # colors
 BACKGROUND = '#aaa5ff'
@@ -15,26 +16,32 @@ WHITE = '#FFFFFF'
 
 class Rectangle:
     def __init__(self, x: int, y: int, width: int, height: int, color: str, border_color=BLACK, border_width=6):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+        self.rect = pygame.Rect(x, y, width, height)
         self.color = color
         self.border_color = border_color
         self.border_width = border_width
+        
     
     def draw(self, screen):
-        rect = (self.x, self.y, self.width, self.height)
-        pygame.draw.rect(screen, self.color, rect)
-        pygame.draw.rect(screen, self.border_color, rect, self.border_width)
+        pygame.draw.rect(screen, self.color, self.rect)
+        pygame.draw.rect(screen, self.border_color, self.rect, self.border_width)
 
-    def contains_rect(self, other):
-        return (
-            other.x >= self.x and
-            other.y >= self.y and
-            other.x + other.width <= self.x + self.width and
-            other.y + other.height <= self.y + self.height
-        )
+    
+def can_move(future_rect, zones):
+    return any(zone.rect.contains(future_rect) for zone in zones)
+
+def move_player(player, dx, dy, zones):
+    if dx != 0:
+        future_pos = player.rect.copy()
+        future_pos.x += dx
+        if can_move(future_pos, zones):
+            player.rect.x += dx
+
+    if dy != 0:
+        future_pos = player.rect.copy()
+        future_pos.y += dy
+        if can_move(future_pos, zones):
+            player.rect.y += dy
 
 
 def main():
@@ -43,45 +50,36 @@ def main():
     clock = pygame.time.Clock()
     running = True
     player = Rectangle(175, 275, 40, 40, RED)
-    rectangles = [
+    zones = [
         Rectangle(120, 160, 150, 265, GREEN), # left green
         Rectangle(742, 160, 150, 265, GREEN), # right green
         Rectangle(315, 198, 385, 187, WHITE), # middle screen
     ]
-
+    
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         
-        screen.fill(BACKGROUND)
-        for rect in rectangles:
-            rect.draw(screen)
-        player.draw(screen)
 
-        speed = 2
         dx = dy = 0
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            dy -= speed
+            dy -= PLAYER_SPEED
         if keys[pygame.K_s]:
-            dy += speed
+            dy += PLAYER_SPEED
         if keys[pygame.K_a]:
-            dx -= speed
+            dx -= PLAYER_SPEED
         if keys[pygame.K_d]:
-            dx += speed
+            dx += PLAYER_SPEED
 
-        if dx != 0:
-            future_pos = Rectangle(player.x + dx, player.y, player.width, player.height, player.color)
-            for rec in rectangles:
-                if rec.contains_rect(future_pos):
-                    player.x += dx
+        move_player(player, dx, dy, zones)
 
-        if dy != 0:
-            future_pos = Rectangle(player.x, player.y + dy, player.width, player.height, player.color)
-            for rec in rectangles:
-                if rec.contains_rect(future_pos):
-                    player.y += dy
+
+        screen.fill(BACKGROUND)
+        for zone in zones:
+            zone.draw(screen)
+        player.draw(screen)
 
         pygame.display.flip()
         clock.tick(FPS)
