db3a36700ec703fd5fbc8c3c1d7e44cbe24f2ada
Imad <imadsshah2004@gmail.com>
2026-09-22 14:12:29 -0400

    stylistic add

--- src/worlds_easiest_game/engine.py
+++ src/worlds_easiest_game/engine.py
@@ -189,7 +189,7 @@ def coin_text(collected, total):
 
 def level_text(number, total):
     '''The bar's middle label: the current level out of the total, as the original shows it.'''
-    return f'{number}/{total}'
+    return f'LEVEL {number}/{total}'
 
 
 def death_text(deaths):
